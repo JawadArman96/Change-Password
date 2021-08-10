8cb89b6c8c1c392a9d8c3688e27a35b4c9674ad2
jawadarman <buetjawadarman@gmail.com>
2021-08-10 17:49:08 +0900

    added REST API and modified README

--- README.md
+++ README.md
@@ -38,3 +38,67 @@ abcdefgABCDEFG012345*,abcdefGPBCDEFG0123*,FALSE
 abcdefgABCDEFG012345*,abcdefGPQCDEFG0123*,TRUE
 ... ... ...
 ```
+
+# requirement
+```
+pip==21.2.2
+flask==2.0.1
+levenshtein==0.12.0
+```
+# used library's brief
+```
+In order to check similarity between old and 
+new password I used levenshtein library which
+use levensthein distance algorithm.
+# Defination
+The Levenshtein distance between two 
+strings a,b (of length |a| and |b| respectively)
+is given by  lev (a,b) where
+
+lev(a,b) = {
+ Case 1:    |a|  , if |b| ==0
+ Case 2:    |b|  , if |a| ==0
+ Case 3:    lev(tail(a), tail(b)), if a[0] == b[0]
+ Case 4:    1 + min (lev(tail(a), b), 
+                     lev(a, tail(b)), 
+                     lev(tail(a), tail(b)))
+                     , otherwise   
+ }
+ 
+The algorithm considers 3 types of operation to
+a string insertions, deletions or substitutions
+each step is consdered with cost of 1. The distance 
+between two string a, b is the minimum number
+of operations needed to generate one string from a to b
+This is implemented using dynamic programming
+techniquie
+ 
+For example we can consider to strings a = "benyam" and 
+b = "ephrem" the minimum distance between these two string 
+is 5 . For DP approach the dp array is calculated 
+in following way
+
+        1   2   3   4   5   6   7  
+        ""  b   e   n   y   a   m
+1   ""  0   1   2   3   4   5   6
+2   e   1   1   1   2   3   4   5
+3   p   2   2   2   2   3   4   5
+4   h   3   3   3   3   3   4   5
+5   r   4   4   4   4   4   4   5
+6   e   5   5   4   5   5   5   5
+7   m   6   6   5   5   6   6   5
+    
+we can create this array in O(|a| * |b|) time.
+The result distance of these two string is 
+array[|a|][|b|] = 5
+so distance = 5
+for matching similarity in percentage I used following
+equation
+
+percentage = (1 - distance/ max(|a|, |b|))%100
+so the similarity between these two string is 
+calculated would be 
+percentage = (1 - 5/6)* 100 %
+=16.66%
+
+```
--- change_password.py
+++ change_password.py
@@ -1,16 +1,35 @@
-#!/usr/bin/env python3
 import re
 from Levenshtein import distance
-import sys
+from flask import Flask
+app = Flask(__name__)
 
 
 saved_password = "abcdefgABCDEFG012345*"
+error_message = ""
+
+
+@app.route("/get-password", methods=['GET'])
+def get():
+    return "old password: " + saved_password
+
+
+@app.route("/set-password/<string:password>", methods=['POST'])
+def set_password(password):
+    message = request_set(password)
+    return message
+
+
+@app.route("/change-password/<string:password>", methods=['POST'])
+def change_password_api(password):
+    message = request_change(password)
+    return message
 
 
 def change_password(old_password, new_password):
+    global error_message
     # ###### Checking with previous password ############
     if new_password == old_password:  # checking if password
-        print("Password matched with the old one")  # is equal
+        error_message = "Password matched with the old one"  # is equal
         return False
     # checking if 80% match with previous
     dist = distance(old_password, new_password)
@@ -18,7 +37,7 @@ def change_password(old_password, new_password):
     percentage = (1 - (dist/max_len)) * 100
     # print(str(dist) + " " + str(max_len) + " " + str(percentage))
     if percentage >= 80:
-        print("password matches 80% with the old one")
+        error_message = "password matches 80% with the old one"
         return False
     #############################################
     # check password with the requirement
@@ -26,6 +45,7 @@ def change_password(old_password, new_password):
 
 
 def check_password(password):
+    global error_message
     special_chars = ['!', '@', '#', '$', '&', '*']
     # define regex (in total min length of the password should be 19)
     reg_ex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$&*])[A-Za-z0-9!@#$&*]{19,}$"
@@ -35,8 +55,8 @@ def check_password(password):
     match = re.search(pattern, password)
     # validating conditions
     if not match:  # checking alphanumeric,digits,
-        print("should be 18 alphanumeric characters, atleast 1 uppercase,"
-              "atleast 1 lowercase, 1 special characters")
+        error_message = "should be 18 alphanumeric characters, atleast " \
+                        "1 uppercase, atleast 1 lowercase, 1 special characters"
         return False  # special characters and length
     # checking repetition
     numeric_count = 0
@@ -44,19 +64,19 @@ def check_password(password):
         char_val = chr(ord('a') + i)  # checking lowercase repeat
         count_val = password.count(char_val)
         if count_val > 4:
-            print("Repeated lowercase 4 times")
+            error_message = "Repeated lowercase 4 times"
             return False
         char_val = chr(ord('A') + i)
         count_val = password.count(char_val)  # checking uppercase repeat
         if count_val > 4:
-            print("Repeated uppercase 4 times")
+            error_message = "Repeated uppercase 4 times"
             return False
         if i < 10:
             char_val = chr(ord('0') + i)
             count_val = password.count(char_val)  # repeating numeric
             numeric_count += count_val
             if count_val > 4:
-                print("Repeated numbers 4 times")
+                error_message = "Repeated numbers 4 times"
                 return False
 
     # checking extra special characters
@@ -64,12 +84,12 @@ def check_password(password):
     for char_val in special_chars:
         count_val += password.count(char_val)
     if count_val > 4:
-        print("More than 4 special characters")
+        error_message = "More than 4 special characters"
         return False
     # checking if 50% is a number
     percentage = (numeric_count / len(password)) * 100
     if percentage >= 50:
-        print("50% of the password should not be number")
+        error_message = "50% of the password should not be number"
         return False
     return True
 
@@ -79,8 +99,23 @@ def request_change(req_password):
     bool_val = change_password(saved_password, req_password)
     if bool_val:
         saved_password = req_password
-        print("Password change successfully")
+        message = "Password change successfully"
+    else:
+        message = error_message
+    return message
+
+
+def request_set(req_password):
+    global saved_password
+    bool_val = check_password(req_password)
+    if bool_val:
+        saved_password = req_password
+        message = "Password set successfully"
+    else:
+        message = error_message
+    return message
 
 
 if __name__ == "__main__":
-    print(change_password(saved_password, sys.argv[1]))
+    app.run(debug=True)
+    # print(change_password(saved_password, sys.argv[1]))
